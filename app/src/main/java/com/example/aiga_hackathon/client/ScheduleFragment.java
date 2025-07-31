c8a13a41cf83be765ffc466ef10d0f3df78c5a10
dosmukhamed <dosmuhamed005@gmail.com>
2025-07-31 14:42:08 +0500

    schedule

--- app/src/main/java/com/example/aiga_hackathon/client/ScheduleFragment.java
+++ app/src/main/java/com/example/aiga_hackathon/client/ScheduleFragment.java
@@ -1,12 +1,11 @@
 package com.example.aiga_hackathon.client;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.aiga_hackathon.R;
 
@@ -16,6 +15,8 @@ import com.example.aiga_hackathon.R;
  * create an instance of this fragment.
  */
 public class ScheduleFragment extends Fragment {
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER

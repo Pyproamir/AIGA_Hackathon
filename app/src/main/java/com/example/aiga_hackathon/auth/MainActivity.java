a9488201da79c362cf36e75b2ca19bc7afb940c7
dosmukhamed <dosmuhamed005@gmail.com>
2025-08-02 05:06:40 +0500

    welcome page update

--- app/src/main/java/com/example/aiga_hackathon/auth/MainActivity.java
+++ app/src/main/java/com/example/aiga_hackathon/auth/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.aiga_hackathon.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.aiga_hackathon.R;
 
@@ -20,22 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        btnSignIn = findViewById(R.id.btn_sign_in);
-        btnSignUp = findViewById(R.id.btn_sign_up);
 
-        btnSignIn.setOnClickListener(v->{
-            startActivity(new Intent(this, SignInActivity.class));
-            finish();
+        FrameLayout mainLayout = findViewById(R.id.main);
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                finish();
+            }
         });
 
-        btnSignUp.setOnClickListener(v->{
-            startActivity(new Intent(this, SignUpActivity.class));
-            finish();
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//        btnSignIn = findViewById(R.id.btn_sign_in);
+//        btnSignUp = findViewById(R.id.btn_sign_up);
+//
+//        btnSignIn.setOnClickListener(v->{
+//            startActivity(new Intent(this, SignInActivity.class));
+//            finish();
+//        });
+//
+//        btnSignUp.setOnClickListener(v->{
+//            startActivity(new Intent(this, SignUpActivity.class));
+//            finish();
+//        });
     }
 }
---
+++ app/src/main/java/com/example/aiga_hackathon/auth/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.aiga_hackathon.auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aiga_hackathon.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_2);
+
+        FrameLayout mainLayout = findViewById(R.id.main);
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, ChoosingAuditory.class));
+                finish();
+            }
+        });
+
+    }
+}
